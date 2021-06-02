3cfcb04c7770ffcf6fb79e8e715be82150a6bffc
Akash <akashpal784@gmail.com>
2021-06-02 13:34:59 +0530

    some small changes

--- uselocalstorage.js
+++ uselocalstorage.js
@@ -1,26 +1,39 @@
-let orguser =document.getElementById("email");
-let orgpass = document.getElementById("password");
+
+
+let orgusername =document.getElementById("email");
+let orgemail = document.getElementById("password");
+let orgphone=document.getElementById("phone");
+let orgpassword=document.getElementById("password");
+let orgcpassword=document.getElementById("cpassword");
 let submit = document.getElementsByClassName("btn");
 
 if(window.localStorage){
-    localStorage.setItem("user","akashpal784@gmail.com");
-    localStorage.setItem("pass" ,"@123")
+    localStorage.setItem("username","user1");
+    localStorage.setItem("email","user1@gmail.com");
+    localStorage.setItem("phone","1234567892");
+    localStorage.setItem("password","user1@123");
+    localStorage.setItem("cpassword","user1@123");
 
-    let user =localStorage.getItem("user");
-    let pass =localStorage.getItem("pass");
+    let username = localStorage.getItem("username");
+    let email = localStorage.getItem("email");
+    let phone = localStorage.getItem("phone");
+    let password = localStorage.getItem("password");
+    let cpassword = localStorage.getItem("cpassword");
 
-    let meassage =document.getElementsByClassName("message");
 
+    let message =document.getElementsByTagName("P");
 
-    submit.addEventListener('click', ()=>{
-        if(user==orguser.value && pass==orgpass.value){
-            message.innerHTML ="LOgin successful...";
+    function show(){
+        if(username==orgusername.value && email==orgemail.value && phone==orgphone.value && password==orgpassword.value && cpassword==orgpassword.value && password==cpassword){
+            message.innerHTML ="Login successful...";
 
         }
         else{
             message.innerHTML="username or password invalid..";
         }
-    })
+
+    }
+    submit.addEventListener('click',show)
 
 
 }
